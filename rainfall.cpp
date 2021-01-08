54d37470a86c7f377a7c94135b16cd04b8edb1be
Michael L. Collard <collard@uakron.edu>
2021-01-08 16:15:08 -0500

    Add header comment

--- rainfall.cpp
+++ rainfall.cpp
@@ -1,3 +1,10 @@
+/*
+    rainfall.cpp
+
+    Rainfall report. Individual rain amounts per hour are input. Output is the average
+    and heaviest hourly rainfall.
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <vector>
